8997f98f97a862c1e0d544d604a29009060573d7
Yash Khandelwal <ykhandelwal158@gmail.com>
2026-06-07 16:31:36 +0530

    ReponseModel

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
-@app.get("/")                                                    
-def home():
-    return {'message' : "This is home route"}
+details = []
 
+class Userdata(BaseModel):
+    id:int
+    name:str
+    age:int
+    address:str
+    password:int
+    accountpassword:int
 
-@app.get("/users")                                               
-def home():
-    return {'message' : "This is users route"}
+class Usershow(BaseModel):
+    id:int
+    name:str
+    age:int
+    address:str
 
-@app.get("/api/fetch")                                           
-def home():
-    return {'message' : "Here /api/fetch is an API Endpoints whereas /fetch is route"}
+@app.post("/users")
+def user_create(user:Userdata):
+    details.append(user)
+    return {
+        "message":"Data Created",
+        "Data":user
+    }
+
+@app.get("/users", response_model=List[Usershow])
+def getuser():
+    return details
+
+@app.get("/users/{user_id}", response_model=Usershow)
+def get_user(user_id:int):
+    for detail in details:
+        if detail.id == user_id:
+            return detail
+    return {
+        "error":"User not found"
+    }
