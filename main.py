1a5722c17f28dae3413a8179f50943981fd4f890
Yash Khandelwal <ykhandelwal158@gmail.com>
2026-06-07 09:48:39 +0530

    queryparameter

--- main.py
+++ main.py
@@ -2,15 +2,38 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")                                                    
-def home():
-    return {'message' : "This is home route"}
+# Query Parameter is used to do searching, sorting and filtering .
 
+# Simple Query Parameter 
 
-@app.get("/users")                                               
-def home():
-    return {'message' : "This is users route"}
+@app.get("/users")                                  # Here /users give you an error
+def users(user:str):                                # But /users?user=xyz is not give you an error
+    return {
+        'message':user
+    }
 
-@app.get("/api/fetch")                                           
-def home():
-    return {'message' : "Here /api/fetch is an API Endpoints whereas /fetch is route"}
+# Optional Query Paramete
+
+@app.get("/user_id")
+def userID(userid:int=None):
+    return {
+        'message':userid
+    }
+
+# Default Values Query Paramete
+
+@app.get("/user_detail")
+def userdetail(age:int=10):
+    return {
+        'message':age
+    }
+
+# Multiple Query Paramete
+
+@app.get("/user_details")
+def userdetails(name:str=None,age:int=0):
+    return {
+        'message':'Userdetails',
+        'name':name,
+        'age':age
+    }
