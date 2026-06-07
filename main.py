cdc49ff2278a65a28cd60abb8d5d571f8e5531e1
Yash Khandelwal <ykhandelwal158@gmail.com>
2026-06-07 10:31:35 +0530

    requestbody

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+# BaseModel class declaration
+
+class userdetails(BaseModel):
+    name:str
+    age:int
+    address:str
+
 
 app = FastAPI()
 
-@app.get("/")                                                    
-def home():
-    return {'message' : "This is home route"}
+# Post request is used send the data on server
+# Except get request , all requests test on swagger ui
+
+# Request body is responsible for handling the data at backend side 
+# when frontend send the data to server using the post request and input method.
 
 
-@app.get("/users")                                               
-def home():
-    return {'message' : "This is users route"}
+# JSON data handling But comes with no data validation so not recommended.
 
-@app.get("/api/fetch")                                           
-def home():
-    return {'message' : "Here /api/fetch is an API Endpoints whereas /fetch is route"}
+@app.post("/create")
+def createdata(data:dict):
+    return {
+        'message':'Data Created',
+        'Data':data
+    }
+
+# To overcome above problem and also manage the data in Json with data validation, use Pydantic model 
+
+@app.post("/createuser")
+def createdata(data:userdetails):
+    return {
+        'message':'Data Created',
+        'Data':data
+    }
