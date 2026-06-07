4c126475b7ea5954e3cbcafb491d3112241c7815
Yash Khandelwal <ykhandelwal158@gmail.com>
2026-06-07 15:14:09 +0530

    Path+Query+Body

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")                                                    
-def home():
-    return {'message' : "This is home route"}
+details = []
 
+class Users(BaseModel):
+    id:int
+    name:str
+    age:int
 
-@app.get("/users")                                               
-def home():
-    return {'message' : "This is users route"}
+# Create Route
 
-@app.get("/api/fetch")                                           
-def home():
-    return {'message' : "Here /api/fetch is an API Endpoints whereas /fetch is route"}
+@app.post("/users")
+def create_user(user:Users):
+    details.append(user)
+    return {
+        "message":"Data Created",
+        "Data":user
+    }
+
+# Update the Data using Query Parameter
+
+@app.put("/users/{user_id}")
+def update_user(user_id:int,updated_data:Users,notify:bool=False):             # Here you can see that, Pathparam, Queryparam and Requestbody are used together
+    for index,detail in enumerate(details):
+        if detail.id == user_id:
+            details[index] = updated_data
+            return {
+                "message":"Data Updated",
+                "notify":notify,
+                "Data":updated_data
+            }
+    return {
+        "error":"User not found"
+    }
