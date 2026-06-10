ddef4f63e01311a7ff6ea863b99277032c2edcb9
Yash Khandelwal <ykhandelwal158@gmail.com>
2026-06-10 23:36:40 +0530

    pytest

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")                                                    
+@app.get("/")
 def home():
-    return {'message' : "This is home route"}
+    return {
+        "message":"Hello Yash !"
+    }
 
+@app.get("/add")
+def add(a:int,b:int):
+    return{
+        "Result":a+b
+    }
 
-@app.get("/users")                                               
-def home():
-    return {'message' : "This is users route"}
-
-@app.get("/api/fetch")                                           
-def home():
-    return {'message' : "Here /api/fetch is an API Endpoints whereas /fetch is route"}
