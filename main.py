f865a8cbc3380acb30058f713bdf454fe8735ed5
Yash Khandelwal <ykhandelwal158@gmail.com>
2026-06-11 16:07:03 +0530

    Cache_API

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 from fastapi import FastAPI
+import requests
+from bs4 import BeautifulSoup
+import time
 
 app = FastAPI()
 
-@app.get("/")                                                    
-def home():
-    return {'message' : "This is home route"}
+#Cache Storage
+cache_data = []
+last_fetch = 0
 
-
-@app.get("/users")                                               
-def home():
-    return {'message' : "This is users route"}
-
-@app.get("/api/fetch")                                           
-def home():
-    return {'message' : "Here /api/fetch is an API Endpoints whereas /fetch is route"}
+@app.get("/news")
+def get_news():
+    global cache_data,last_fetch
+    start = time.time()
+    if time.time() - last_fetch > 60:
+        print("Fetching Fresh Data")
+        url = "https://news.ycombinator.com/"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text,"html.parser")
+        cache_data = [
+            item.text for item in soup.find_all("span",class_="titleline")
+        ]
+        last_fetch = time.time()
+    else:
+        print("Using catche Data")
+    
+    end = time.time()
+    time_taken = round(end-start,4)
+    print("Time Taken: ",time_taken)
+    return {
+        "Time Taken":time_taken,
+        "Data":cache_data[:5]
+    }
