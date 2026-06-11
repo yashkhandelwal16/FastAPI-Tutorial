4b815ede61b6c9198db19b12f610fe6ada258456
Yash Khandelwal <ykhandelwal158@gmail.com>
2026-06-11 15:34:39 +0530

    thirdpartyAPI

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import requests
 
 app = FastAPI()
 
-@app.get("/")                                                    
-def home():
-    return {'message' : "This is home route"}
+# Get all data 
+@app.get("/posts")
+def get_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    return response.json()
 
-
-@app.get("/users")                                               
-def home():
-    return {'message' : "This is users route"}
-
-@app.get("/api/fetch")                                           
-def home():
-    return {'message' : "Here /api/fetch is an API Endpoints whereas /fetch is route"}
+# Get single post 
+@app.get("/posts/{posts_id}")
+def get_post(posts_id:int):
+    url = f"https://jsonplaceholder.typicode.com/posts/{posts_id}"
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise HTTPException(
+            status_code=404,
+            detail="Page Not Found"
+        )
+    return response.json()
